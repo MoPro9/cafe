591fbe700bd09a089c743d896d266c4510423dc4
MoPro9 <djebbarihatemofficial@gmail.com>
2026-09-05 16:30:47 +0100

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// عوّض هذه القيم بمعطيات مشروعك من Firebase Console
+// (Project settings -> Your apps -> SDK setup and configuration)
+const firebaseConfig = {
+  apiKey: "AIzaSyBJBO7IiPbtlqoiWWg_ex9LyGDCjjmYKAQ",
+  authDomain: "cafe-79531.firebaseapp.com",
+  projectId: "cafe-79531",
+  storageBucket: "cafe-79531.firebasestorage.app",
+  messagingSenderId: "123191058685",
+  appId: "1:123191058685:web:1fe3c4d01bf72a47d49e94",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
